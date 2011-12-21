db93254b2552677db6384ef501c5c1efee1b34ac
idarfan <mr.idarfan@gmail.com>
2011-12-21 17:51:58 +0800

    可惡，把符合學生id的課表都給我列出來

--- app/controllers/kojenadults_controller.rb
+++ app/controllers/kojenadults_controller.rb
@@ -15,7 +15,7 @@ class KojenadultsController < ApplicationController
   # GET /kojenadults/1.xml
   def show
     @kojenadult = Kojenadult.find(params[:id])
-    @kojenadult_classes = KojenadultClasse.all
+    @kojenadult_classes = KojenadultClasse.where("student_id = #{＠kojenadult.student_id}")
     @adults_graduateds = AdultsGraduated.all
     
 
